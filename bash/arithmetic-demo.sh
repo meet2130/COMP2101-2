9505c7f8b6a21e5983c0ffd38ecc2bc2991d5cfe
khpatel2205 <56008068+khpatel2205@users.noreply.github.com>
2019-10-01 17:41:42 -0400

    Update arithmetic-demo.sh

--- bash/arithmetic-demo.sh
+++ bash/arithmetic-demo.sh
@@ -9,14 +9,34 @@
 #   - first divided by second gives X with a remainder of Y
 # Improve this script by calculating and displaying the first number raised to the power of the second number
 
-firstnum=5
-secondnum=2
-sum=$((firstnum + secondnum))
-dividend=$((firstnum / secondnum))
-fpdividend=$(awk "BEGIN{printf \"%.2f\", $firstnum/$secondnum}")
+#Varaiables and data
+#=======================
+#firstnum=5
+#secondnum=2
 
+read -p "Tell me a number: " firstnum
+read -p "Tell me another number: " secondnum
+#========================
+
+#arithmetic
+#============
+sum=$((firstnum + secondnum))
+diff=$((firstnum - secondnum))
+multi=$((firstnum * secondnum))
+dividend=$((firstnum / secondnum))
+modul=$((firstnum % secondnum))
+fpdividend=$(awk "BEGIN{printf \"%.2f\", $firstnum/$secondnum}")
+power=$((firstnum ** secondnum))
+#===============
+
+#Report
+#=============
 cat <<EOF
 $firstnum plus $secondnum is $sum
-$firstnum divided by $secondnum is $dividend
+$firstnum minus $secondnum is $diff
+$firstnum multiplication $secondnum is $multi
+$firstnum divided by $secondnum is $dividend with a remainder of $modul
   - More precisely, it is $fpdividend
+$firstnum raised by $secondnum is $power
 EOF
+#=========================
